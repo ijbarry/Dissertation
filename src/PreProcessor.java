06e623948b1d022911cd7bdf5d4e470a50c041d0
ijbar <ijb29@cam.ac.uk>
2020-12-06 16:01:11 +0000

    Initial commit

---
+++ src/PreProcessor.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
+public class PreProcessor {
+
+    public static void main(String[] args) {
+        int dur=1;
+        int proto=2;
+        int service=3;
+        int state=4;
+        int dpkts=6;
+        int sbytes=7;
+        int dttl=11;
+        int sjit=18;
+        int stcpb=21;
+        int ackdat=26;
+        int smean=27;
+        int dmean=28;
+        int ct_state_ttl=32;
+        int ct_dst_src_ltm=36;
+
+        try {
+            File trainingSet = new File("UNSW_NB15_training-set.csv");
+            File reducedTrainingSet = new File("reduced_training-set.csv");
+            FileWriter fileWriter = new FileWriter("reduced_training-set.csv");
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            Scanner scanner = new Scanner(trainingSet);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                String[] datum = data.split(",");
+                bw.write(datum[dur]+","+datum[proto]+","+datum[service]+","+
+                        datum[state]+","+datum[dpkts]+","+datum[sbytes]+","+datum[dttl]+","+
+                        datum[sjit]+","+datum[stcpb]+","+datum[ackdat]+","+datum[smean]+","+
+                        datum[dmean]+","+datum[ct_state_ttl]+","+datum[ct_dst_src_ltm]);
+                bw.newLine();
+            }
+            scanner.close();
+            bw.flush();
+            bw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException.");
+            e.printStackTrace();
+        } catch (IOException e2) {
+            System.out.println("IOException.");
+            e2.printStackTrace();
+        }
+    }
+    }
